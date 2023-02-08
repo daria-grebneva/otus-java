cdf80f4214094cbc418cc04c404e0735f59826f7
daria-grebneva <greda2598@gmail.com>
2023-02-08 23:17:59 +0300

    Поменяла тип данных на примитивы

--- hw04-gc/src/main/java/ru/calculator/CalcDemo.java
+++ hw04-gc/src/main/java/ru/calculator/CalcDemo.java
@@ -10,15 +10,21 @@ package ru.calculator;
 -Xlog:gc=debug:file=./logs/gc-%p-%t.log:tags,uptime,time,level:filecount=5,filesize=10m
 */
 
+import java.time.LocalDateTime;
+
 public class CalcDemo {
     public static void main(String[] args) {
-        long counter = 1_000_000;
+        long counter = 10_000_000;
         var summator = new Summator();
         long startTime = System.currentTimeMillis();
 
         for (var idx = 0; idx < counter; idx++) {
             var data = new Data(idx);
             summator.calc(data);
+
+            if (idx % 10_000_000 == 0) {
+                System.out.println(LocalDateTime.now() + " current idx:" + idx);
+            }
         }
 
         long delta = System.currentTimeMillis() - startTime;
