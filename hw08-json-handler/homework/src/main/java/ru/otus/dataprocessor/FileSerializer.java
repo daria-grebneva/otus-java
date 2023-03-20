76508e761c7c85828484cd46b6c0af44a3cd9162
daria-grebneva <greda2598@gmail.com>
2023-03-20 23:51:56 +0300

    ДЗ на тему: "Обработчик json-ов"

--- hw08-json-handler/homework/src/main/java/ru/otus/dataprocessor/FileSerializer.java
+++ hw08-json-handler/homework/src/main/java/ru/otus/dataprocessor/FileSerializer.java
@@ -1,14 +1,25 @@
 package ru.otus.dataprocessor;
 
+import com.google.gson.Gson;
+
+import java.io.*;
 import java.util.Map;
 
 public class FileSerializer implements Serializer {
+    private final String fileName;
 
     public FileSerializer(String fileName) {
+        this.fileName = fileName;
     }
 
     @Override
     public void serialize(Map<String, Double> data) {
-        //формирует результирующий json и сохраняет его в файл
+        var gson = new Gson();
+        String json = gson.toJson(data);
+        try (var bufferedWriter = new BufferedWriter(new FileWriter(fileName))) {
+            bufferedWriter.write(json);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
