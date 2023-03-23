b9893bf0c34bc3697e2c91160ca847c9bb70e0c9
daria-grebneva <greda2598@gmail.com>
2023-03-23 23:05:24 +0300

    Правки по ДЗ "Эмулятор банкомата"

--- hw06-atm/src/main/java/ru/otus/AtmImpl.java
+++ hw06-atm/src/main/java/ru/otus/AtmImpl.java
@@ -21,60 +21,7 @@ public class AtmImpl implements Atm {
         if (amount == 0) {
             throw new RuntimeException("Initial amount can't be equal to 0");
         }
-        return getMoneyFromAtmRecursively(new ArrayList<>(), amount);
-    }
-
-    private List<Banknote> getMoneyFromAtmRecursively(List<Banknote> money, int resultAmount) {
-        if (resultAmount == 0) {
-            return money;
-        } else if (resultAmount < 0) {
-            recoverMoneyBack(money);
-            throw new RuntimeException("There is exception with your amount in ATM");
-        }
-        if (getAtmBalance() <= 0) {
-            throw new RuntimeException("There is no money in ATM");
-        }
-
-        Banknote finalTempBanknote = getClosestToAmountBanknote(resultAmount);
-        if (finalTempBanknote == null) {
-            recoverMoneyBack(money);
-            throw new RuntimeException("There is exception with your amount in ATM");
-        }
-
-        resultAmount = decreaseBanknoteQuantityAndAddItIntoList(money, finalTempBanknote, resultAmount);
-
-        return getMoneyFromAtmRecursively(money, resultAmount);
-    }
-
-    private int decreaseBanknoteQuantityAndAddItIntoList(List<Banknote> money, Banknote finalTempBanknote, int resultAmount) {
-        for (AtmCell cell : moneyVault.getAtmCellList()) {
-            if (cell.getBanknote().equals(finalTempBanknote)) {
-                money.add(finalTempBanknote);
-                cell.setBanknoteQuantity(cell.getBanknoteQuantity() - 1);
-                resultAmount = resultAmount - finalTempBanknote.getDenomination().getValue();
-            }
-        }
-
-        return resultAmount;
-    }
-
-    private void recoverMoneyBack(List<Banknote> money) {
-        for (Banknote banknote : money) {
-            moneyVault.getAtmCellList().stream()
-                    .filter(c -> c.getBanknote().equals(banknote))
-                    .findFirst()
-                    .ifPresent(cell -> cell.setBanknoteQuantity(cell.getBanknoteQuantity() + 1));
-        }
-    }
-
-    private Banknote getClosestToAmountBanknote(int amount) {
-        Banknote tempBanknote = null;
-        for (AtmCell cell : moneyVault.getAtmCellList()) {
-            if (cell.getBanknoteDenomination().getValue() <= amount && cell.getBanknoteQuantity() > 0) {
-                tempBanknote = cell.getBanknote();
-            }
-        }
-        return tempBanknote;
+        return moneyVault.getMoneyFromAtmRecursively(new ArrayList<>(), amount);
     }
 
     @Override
--- hw06-atm/src/main/java/ru/otus/MoneyVault.java
+++ hw06-atm/src/main/java/ru/otus/MoneyVault.java
@@ -1,6 +1,7 @@
 package ru.otus;
 
 import java.util.Comparator;
+import java.util.List;
 import java.util.TreeSet;
 
 public class MoneyVault {
@@ -21,5 +22,58 @@ public class MoneyVault {
 
     public int getMoneyVaultBalance() {
         return atmCellList.stream().mapToInt(c -> c.getBanknoteDenomination().getValue() * c.getBanknoteQuantity()).sum();
+    }
+
+    public List<Banknote> getMoneyFromAtmRecursively(List<Banknote> money, int resultAmount) {
+        if (resultAmount == 0) {
+            return money;
+        } else if (resultAmount < 0) {
+            recoverMoneyBack(money);
+            throw new RuntimeException("There is exception with your amount in ATM");
+        }
+        if (getMoneyVaultBalance() <= 0) {
+            throw new RuntimeException("There is no money in ATM");
+        }
+
+        Banknote finalTempBanknote = getClosestToAmountBanknote(resultAmount);
+        if (finalTempBanknote == null) {
+            recoverMoneyBack(money);
+            throw new RuntimeException("There is exception with your amount in ATM");
+        }
+
+        resultAmount = decreaseBanknoteQuantityAndAddItIntoList(money, finalTempBanknote, resultAmount);
+
+        return getMoneyFromAtmRecursively(money, resultAmount);
+    }
+
+    private int decreaseBanknoteQuantityAndAddItIntoList(List<Banknote> money, Banknote finalTempBanknote, int resultAmount) {
+        for (AtmCell cell : getAtmCellList()) {
+            if (cell.getBanknote().equals(finalTempBanknote)) {
+                money.add(finalTempBanknote);
+                cell.setBanknoteQuantity(cell.getBanknoteQuantity() - 1);
+                resultAmount = resultAmount - finalTempBanknote.getDenomination().getValue();
+            }
+        }
+
+        return resultAmount;
+    }
+
+    private void recoverMoneyBack(List<Banknote> money) {
+        for (Banknote banknote : money) {
+            getAtmCellList().stream()
+                    .filter(c -> c.getBanknote().equals(banknote))
+                    .findFirst()
+                    .ifPresent(cell -> cell.setBanknoteQuantity(cell.getBanknoteQuantity() + 1));
+        }
+    }
+
+    private Banknote getClosestToAmountBanknote(int amount) {
+        Banknote tempBanknote = null;
+        for (AtmCell cell : getAtmCellList()) {
+            if (cell.getBanknoteDenomination().getValue() <= amount && cell.getBanknoteQuantity() > 0) {
+                tempBanknote = cell.getBanknote();
+            }
+        }
+        return tempBanknote;
     }
 }
