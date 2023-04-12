caca5c63ff665027820fa361a2a0116ac97a7ab6
daria-grebneva <greda2598@gmail.com>
2023-04-12 22:12:43 +0300

    Добавила внешние ключи в базу

--- hw10-hibernate/homework/src/main/resources/db/migration/V1__initial_schema.sql
+++ hw10-hibernate/homework/src/main/resources/db/migration/V1__initial_schema.sql
@@ -1,24 +1,27 @@
-
--- Для @GeneratedValue(strategy = GenerationType.SEQUENCE)
-create sequence client_SEQ start with 1 increment by 1;
-
-create table client
-(
-    id   bigint not null primary key,
-    name varchar(50)
-);
-
--- Для @GeneratedValue(strategy = GenerationType.IDENTITY)
-
 create table address
 (
     id   bigserial not null primary key,
     street varchar(255)
 );
 
+create sequence client_SEQ start with 1 increment by 1;
+
+create table client
+(
+    id   bigint not null primary key,
+    name varchar(50),
+    address_id bigserial,
+    constraint fk_address
+            foreign key(address_id)
+                references address(id)
+);
+
 create table phone
 (
     id   bigserial not null primary key,
     number varchar(25),
-    client_id bigint
+    client_id bigint,
+    constraint fk_client
+        foreign key(client_id)
+            references client(id)
 );
