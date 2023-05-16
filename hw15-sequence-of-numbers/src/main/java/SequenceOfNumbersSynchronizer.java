41782a92bbf9662efb7db940ce4e44465b988a2b
daria-grebneva <greda2598@gmail.com>
2023-05-16 23:45:57 +0300

    Правки:  - Потоки теперь чередуются в выводе  - Бесконечный цикл вывода значений

--- hw15-sequence-of-numbers/src/main/java/SequenceOfNumbersSynchronizer.java
+++ hw15-sequence-of-numbers/src/main/java/SequenceOfNumbersSynchronizer.java
@@ -3,45 +3,74 @@ import org.slf4j.LoggerFactory;
 
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ExecutionException;
 import java.util.concurrent.TimeUnit;
 
 public class SequenceOfNumbersSynchronizer {
     private static final Logger LOGGER = LoggerFactory.getLogger(SequenceOfNumbersSynchronizer.class);
-    private final Lock lock = new ReentrantLock();
+    private final Lock LOCK = new ReentrantLock();
+    private final ExecutorService EXECUTOR = Executors.newFixedThreadPool(2);
 
-    public static void main(String[] args) throws InterruptedException {
+    public static void main(String[] args) throws InterruptedException, ExecutionException {
         new SequenceOfNumbersSynchronizer().sequenceThreads();
     }
 
-    private void sequenceThreads() throws InterruptedException {
-        var t1 = new Thread(this::task);
-        t1.setName("t1");
-        t1.start();
+    private void sequenceThreads() throws InterruptedException, ExecutionException {
 
-        var t2 = new Thread(this::task);
-        t2.setName("t2");
-        t2.start();
+        var result = new Object() {
+            int value1 = 1;
+            int value2 = 1;
+        };
 
-        t1.join();
-        t2.join();
+        do {
+            if (result.value1 == 1 && result.value2 == 1) {
+                do {
+                    result.value1 = getIncrementedValue(result.value1);
+                    result.value2 = getIncrementedValue(result.value2);
+                } while (result.value1 != 10 && result.value2 != 10);
+            }
+
+            result.value1 = getDecrementedValue(result.value1);
+            result.value2 = getDecrementedValue(result.value2);
+        }
+        while (result.value1 != 0 && result.value2 != 0);
+
+        EXECUTOR.shutdown();
     }
 
-    private synchronized void task() {
-        lock.lock();
-        int counter = 1;
-        while (counter < 10) {
+    private int getDecrementedValue(int result) throws ExecutionException, InterruptedException {
+        return EXECUTOR.submit(() -> decrement(result)).get();
+    }
+
+    private int getIncrementedValue(int result) throws ExecutionException, InterruptedException {
+        return EXECUTOR.submit(() -> increment(result)).get();
+    }
+
+    private int increment(int counter) {
+        LOCK.lock();
+        if (counter < 10) {
             LOGGER.info(String.valueOf(counter++));
         }
-        while (counter > 0) {
+        sleep();
+        LOCK.unlock();
+        return counter;
+    }
+
+    private int decrement(int counter) {
+        LOCK.lock();
+        if (counter > 0) {
             LOGGER.info(String.valueOf(counter--));
         }
         sleep();
-        lock.unlock();
+        LOCK.unlock();
+        return counter;
     }
 
     private static void sleep() {
         try {
-            Thread.sleep(TimeUnit.SECONDS.toMillis(5));
+            Thread.sleep(TimeUnit.MILLISECONDS.toMillis(1));
         } catch (InterruptedException e) {
             Thread.currentThread().interrupt();
         }
