f14e48ea999cf0a39862e38916f9b5519922416d
daria-grebneva <greda2598@gmail.com>
2023-05-18 23:55:14 +0300

    Поменяла реализацию  - Начинает теперь гарантированно первый поток (контролируется с помощью wait)  - counter - общий для обоих потоков

--- hw15-sequence-of-numbers/src/main/java/SequenceOfNumbersSynchronizer.java
+++ hw15-sequence-of-numbers/src/main/java/SequenceOfNumbersSynchronizer.java
@@ -1,76 +1,67 @@
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.util.concurrent.locks.Lock;
-import java.util.concurrent.locks.ReentrantLock;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.ExecutionException;
 import java.util.concurrent.TimeUnit;
 
 public class SequenceOfNumbersSynchronizer {
     private static final Logger LOGGER = LoggerFactory.getLogger(SequenceOfNumbersSynchronizer.class);
-    private final Lock LOCK = new ReentrantLock();
-    private final ExecutorService EXECUTOR = Executors.newFixedThreadPool(2);
+    public static final String FIRST = "first";
+    public static final String SECOND = "second";
+    private String threadMarker = SECOND;
+    private boolean shouldInc = true;
 
-    public static void main(String[] args) throws InterruptedException, ExecutionException {
-        new SequenceOfNumbersSynchronizer().sequenceThreads();
+
+    public static void main(String[] args) {
+        SequenceOfNumbersSynchronizer sequence = new SequenceOfNumbersSynchronizer();
+        Thread thread1 = new Thread(() -> sequence.action(FIRST));
+        Thread thread2 = new Thread(() -> sequence.action(SECOND));
+
+        thread1.setName("First");
+        thread2.setName("Second");
+
+        thread1.start();
+        thread2.start();
     }
 
-    private void sequenceThreads() throws InterruptedException, ExecutionException {
+    private synchronized void action(String currentThread) {
+        int counter = 1;
 
-        var result = new Object() {
-            int value1 = 1;
-            int value2 = 1;
-        };
+        while (!Thread.currentThread().isInterrupted()) {
+            try {
+                while (threadMarker.equals(currentThread)) {
+                    this.wait();
+                }
 
-        do {
-            if (result.value1 == 1 && result.value2 == 1) {
-                do {
-                    result.value1 = getIncrementedValue(result.value1);
-                    result.value2 = getIncrementedValue(result.value2);
-                } while (result.value1 != 10 && result.value2 != 10);
+                LOGGER.info(String.valueOf(counter));
+
+                if (shouldInc) {
+                    counter++;
+                } else {
+                    counter--;
+                }
+
+                threadMarker = currentThread;
+
+                // indicator to change increment to decrement and vice versa
+                if (threadMarker.equals(SECOND)) {
+                    if (counter >= 10) {
+                        shouldInc = false;
+                    } else if (counter <= 1) {
+                        shouldInc = true;
+                    }
+                }
+
+                sleep();
+                notifyAll();
+            } catch (InterruptedException ex) {
+                Thread.currentThread().interrupt();
             }
-
-            result.value1 = getDecrementedValue(result.value1);
-            result.value2 = getDecrementedValue(result.value2);
         }
-        while (result.value1 != 0 && result.value2 != 0);
-
-        EXECUTOR.shutdown();
-    }
-
-    private int getDecrementedValue(int result) throws ExecutionException, InterruptedException {
-        return EXECUTOR.submit(() -> decrement(result)).get();
-    }
-
-    private int getIncrementedValue(int result) throws ExecutionException, InterruptedException {
-        return EXECUTOR.submit(() -> increment(result)).get();
-    }
-
-    private int increment(int counter) {
-        LOCK.lock();
-        if (counter < 10) {
-            LOGGER.info(String.valueOf(counter++));
-        }
-        sleep();
-        LOCK.unlock();
-        return counter;
-    }
-
-    private int decrement(int counter) {
-        LOCK.lock();
-        if (counter > 0) {
-            LOGGER.info(String.valueOf(counter--));
-        }
-        sleep();
-        LOCK.unlock();
-        return counter;
     }
 
     private static void sleep() {
         try {
-            Thread.sleep(TimeUnit.MILLISECONDS.toMillis(1));
+            Thread.sleep(TimeUnit.MILLISECONDS.toMillis(500));
         } catch (InterruptedException e) {
             Thread.currentThread().interrupt();
         }
